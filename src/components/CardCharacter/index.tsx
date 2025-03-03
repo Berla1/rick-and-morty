0af97d2fb7e8cd309f3b47b470463801bbbd2a16
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2025-03-03 19:09:59 -0300

    arrumando cards para mobile

--- src/components/CardCharacter/index.tsx
+++ src/components/CardCharacter/index.tsx
@@ -20,11 +20,11 @@ const CardCharacter: React.FC<CardCharacterProps> = ({
   origin,
 }) => {
   return (
-      <div className="flex flex-col bg-[#333] rounded-xl justify-center items-center w-full md:flex-row md:w-150 md:justify-normal">
-        <img src={image} alt={name} className="w- h-full rounded-b-xl md:rounded-l-xl" />
-        <div className="flex flex-col px-4">
-          <h2 className="text-2xl pt-4  font-bold">{name}</h2>
-          <p className="flex items-center md:items-start">
+      <div className="flex flex-col bg-[#333] rounded-xl justify-center items-center text-center w-screen md:flex-row md:w-150 md:justify-normal">
+        <img src={image} alt={name} className="w-fit h-full rounded-b-xl md:rounded-l-xl" />
+        <div className="flex flex-col px-4 ">
+          <h2 className="text-2xl pt-4 font-bold">{name}</h2>
+          <p className="flex items-center text-center justify-center md:items-start">
             <PiDotOutlineFill
               size={25}
               fill={
